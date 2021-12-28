eb8d4b389547b9cc9c8b027bbb4f2144a4bd8fc6
ASWINI JENA <71932481+aswinijena3459@users.noreply.github.com>
2021-12-28 16:27:19 +0530

    Add files via upload

---
+++ student placement predictor/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    cgpa = request.form.get('cgpa')
+    iq = request.form.get('iq')
+    profile_score = request.form.get('profile_score')
+
+    input_query = np.array([[cgpa,iq,profile_score]])
+
+    model = pickle.load(open('model.pkl', 'rb'))
+
+    result = model.predict(input_query)[0]
+
+    return jsonify({'placement':str(result)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
